f3570b7460225a628d49d85c74ebbf88f6a0a632
bcg5039 <bcg5039@gmail.com>
2017-06-15 13:08:33 -0600

    added hello container

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -3,8 +3,7 @@ export default props => {
   return (
     <div className="pa2 helvetica bg-black white">
       <a href="https://github.com/bcg5039">
-        Copyright Brett Goers™
-        LLC
+        Copyright Brett Goers™ LLC
       </a>
     </div>
   )
--- src/containers/header.js
+++ src/containers/header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Hello from './hello'
 export default props => {
   return (
     <div className="pa2 helvetica bg-black white">
       THIS IS A GNARLY HEADER!!!
+      <Hello firstName="Sampson" />
     </div>
   )
 }
