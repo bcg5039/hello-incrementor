779831a84464fccf39022ac2e18d39974fb1362a
bcg5039 <bcg5039@gmail.com>
2017-06-15 09:55:11 -0600

    added title, nav, home, and routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import './App.css'
+import Home from './pages/home'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Home} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      </BrowserRouter>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/containers/nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+export default props => {
+  //run some other code here
+  return (
+    <div>
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
